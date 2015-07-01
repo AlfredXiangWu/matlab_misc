1c3c0530ecf0903967d2c0f105c64697426adbff
Alfred Xiang Wu <alfredxiangwu@gmail.com>
2015-07-01 15:46:36 +0800

    IJB-A dataset verification data preprocess init

---
+++ face_verification/data_preprocessing/IJB-A/face_verif_dataset_gen.m
@@ -0,0 +1,7 @@
+%% This program is used to crop the face images from IJB-A dataset for each split of verificaition tasks.
+function [] = face_verif_dataset_gen(img_data_path, meta_data_path, save_data_path)
+    
+
+
+
+end
